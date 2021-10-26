d3102891a3f297a9d20b433185454b4f4428b97f
Pranay Bathini <pranaygoud36@gmail.com>
2021-10-26 23:45:57 +0530

    Update FindMinInSortedArray.java

--- Arrays/FindMinInSortedArray.java
+++ Arrays/FindMinInSortedArray.java
@@ -1,5 +1,23 @@
 //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 class Solution {
+    
+        public int findMinSol(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        
+       int lo=0,hi=nums.length-1;
+
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]>nums[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        
+        return nums[lo];
+    }
+}
+
     public int findMin(int[] nums) {
         if(nums.length == 1){
             return nums[0];
