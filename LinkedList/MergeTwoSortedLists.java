fc3372448b38c867215f449da5cfaff58a13ad89
Pranay Bathini <pranaygoud36@gmail.com>
2021-12-20 11:05:52 +0530

    Update MergeTwoSortedLists.java

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -10,51 +10,35 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            if(l1 == null ){
-                return l2;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
+        }
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
+        while(list1!=null && list2!=null){
+            if(list1.val < list2.val){
+                curr.next = list1;
+                list1= list1.next;
+            }else{
+                curr.next = list2;
+                list2= list2.next;
             }
-            if(l2 == null){
-                return l1;
-            }
-        
-            ListNode start = null;
-            ListNode result = null;
-        
-            while(l1!=null && l2!=null){
-                
-                if(l1.val < l2.val){
-                    if(start == null){
-                        start = l1;
-                        result = l1;
-                        l1 = l1.next;
-                    }else{
-                    start.next = l1;
-                    start = start.next;
-                    l1 = l1.next;
-                  }
-                }else{
-                    if(start == null){
-                        start = l2;
-                         result = l2;
-                        l2 = l2.next;
-                    }else{
-                    start.next = l2;
-                    start = start.next;
-                    l2 = l2.next;
-                    }
-                    
-                }
-            }
-        
-        if(l1== null){
-            start.next = l2;
+            curr = curr.next;
         }
         
-        if(l2 == null){
-            start.next = l1;
-        }
+         if(list1!=null){
+             curr.next=list1;
+         }
         
-        return result;
+        
+         if(list2!=null){
+             curr.next=list2;
+         }
+        
+        return head.next;
     }
 }
