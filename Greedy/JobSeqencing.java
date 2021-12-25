fd64e494a7b8e5a17439b01c175c64396f125bce
Pranay Bathini <pranaygoud36@gmail.com>
2021-12-25 17:00:35 +0530

    Update JobSeqencing.java

--- Greedy/JobSeqencing.java
+++ Greedy/JobSeqencing.java
@@ -1,3 +1,27 @@
+// { Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
             int n = Integer.parseInt(inputLine[0]);
             Job[] arr = new Job[n];
             inputLine = br.readLine().trim().split(" ");
@@ -17,6 +41,62 @@
 }// } Driver Code Ends
 
 
+//            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        Arrays.sort(arr, (a,b)->(b.profit-a.profit));
+        
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i].deadline > max){
+                max = arr[i].deadline;
+            }
+        }
+        //for storing the job sequence order
+        int res[] = new int[max+1];
+        
+        for(int i=1;i<=max;i++){
+            res[i] =-1;
+        }
+        
+        int count=0, profit=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].deadline;j>0;j--){
+                if(res[j]==-1){
+                    res[j] = arr[i].id;
+                    count++;
+                    profit += arr[i].profit;
+                    break;
+                }
+            }
+        }
+        
+        return new int[]{count,profit};
+    }
+}
 class Solution
 {
     //Function to find the maximum profit and the number of jobs done.
