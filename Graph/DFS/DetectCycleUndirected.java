581a8a869fdbccc247e5cb243d155399101125e4
Pranay Bathini <pranaygoud36@gmail.com>
2022-03-11 18:04:50 +0530

    Create DetectCycleUndirected.java

---
+++ Graph/DFS/DetectCycleUndirected.java
@@ -0,0 +1,28 @@
+boolean vis[] = new boolean[v];
+    Arrays.fill(vis,false);
+  
+    for(int i=0;i<v;i++){
+      if(vis[i] == false){ 
+          if(checkForCycle(i,vis,adj,-1)){
+            return true; 
+          }
+      }
+    }
+  return false;
+}
+
+//TC - O(V+E), SC-(V+E) + O(V) for visited array + O(V) for recursive stack
+
+public boolean checkForCycle(int n, boolean[] vis, ArrayList<ArrayList<Integer>> adj,int parent){
+  vis[n] = true;
+  for(int i: adj.get(n)){
+    if(vis[i] == false){
+        if(checkForCycle(i, vis, adj, n)){
+            return true;
+        }
+    }else if(i!=parent) return true;
+    
+  }
+  return false;
+  
+}
