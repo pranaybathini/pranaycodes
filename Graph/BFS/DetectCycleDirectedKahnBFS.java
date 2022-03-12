10a2e763c6f18678dfa37ea78c01b2a8c85e1476
Pranay Bathini <pranaygoud36@gmail.com>
2022-03-12 12:24:53 +0530

    Create DetectCycleDirectedKahnBFS.java

---
+++ Graph/BFS/DetectCycleDirectedKahnBFS.java
@@ -0,0 +1,40 @@
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static boolean topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        int[] topo = new int[V];
+        int[] indegree = new int[V];
+        
+        for(int i=0;i<V;i++){
+            for(int ad : adj.get(i)){
+                indegree[ad]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)q.add(i);
+        }
+        
+        int count = 0;
+        int ind=0;
+        
+        while(!q.isEmpty()){
+            Integer node  = q.poll();
+            topo[ind++] = node;
+          count++;
+            
+            for(int ad : adj.get(node)){
+                indegree[ad]--;
+                if(indegree[ad] == 0) q.add(ad);
+            }
+            
+        }
+      
+        if(count == V) return false; // no cycle as topo output doesn't contain all nodes
+        
+        return true; // cycle exists
+    }
+}
