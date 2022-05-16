1ffee65acbbbaa7b76778e8ae771c2c098e573ac
Pranay Bathini <pranaygoud36@gmail.com>
2022-05-16 15:47:57 +0530

    Create RotateImage.java

---
+++ Arrays/RotateImage.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/rotate-image/
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i=0;i<matrix.length;i++){
+            int j = 0, k= matrix[0].length-1;
+            while(j<k){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+                j++;k--;
+            }
+        }
+    }
+}
