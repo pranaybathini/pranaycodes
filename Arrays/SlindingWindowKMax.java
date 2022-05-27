37c6ec4d94f7c5bb36aa3f53deae98d45562d703
Pranay Bathini <pranaygoud36@gmail.com>
2022-05-27 13:30:29 +0530

    Create SlindingWindowKMax.java

---
+++ Arrays/SlindingWindowKMax.java
@@ -0,0 +1,24 @@
+//Slinding Window of size k - return max array of every window of size k
+ public static int[] maxSlidingWindow(int[] a, int k) {
+        int n = a.length;
+        int[] r = new int[n - k + 1];
+        int ri = 0;
+        // store index
+        Deque < Integer > q = new ArrayDeque < > ();
+        for (int i = 0; i < a.length; i++) {
+            // remove numbers out of range k
+            if (!q.isEmpty() && q.peek() == i - k) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                q.pollLast();
+            }
+
+            q.offer(i);
+            if (i >= k - 1) {
+                r[ri++] = a[q.peek()];
+            }
+        }
+        return r;
+    }
