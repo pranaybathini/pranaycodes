7971c7f2dc0f275d638136d615a3cb7662ee48f7
Pranay Bathini <pranaygoud36@gmail.com>
2022-05-31 21:39:39 +0530

    Update LargeIsland.java

--- Graph/DFS/LargeIsland.java
+++ Graph/DFS/LargeIsland.java
@@ -1,4 +1,5 @@
 //Brute force = dfs- tle
+//https://leetcode.com/problems/making-a-large-island
 class Solution {
     public int largestIsland(int[][] grid) {
         int max = 0;
@@ -31,3 +32,73 @@ class Solution {
         return 1 + a +b+c+d;
     }
 }
+
+//accepted - O(n^2)
+class Solution {
+    
+    static int dr[] = {-1,1,0,0};
+    static int dc[] = {0,0,1,-1};
+    
+    public int largestIsland(int[][] grid) {
+        
+        int row = grid.length, col = grid[0].length;
+        Map<Integer, Integer> areaMap = new HashMap<>();
+        int currIndex = 3, maxArea = 0;
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == 1){
+                    areaMap.put(currIndex, dfs(i,j,grid,row,col,currIndex));
+                    maxArea = Math.max(maxArea, areaMap.get(currIndex++));   
+                }
+            }
+        }
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == 0){
+                    Set<Integer> indexSet = new HashSet<>();
+                    int area = 1;
+                    for(int k=0;k<4;k++){
+                        int x = i+dr[k];
+                        int y = j+dc[k];
+                        if(isValid(x,y,grid,row,col)){
+                            int index = grid[x][y];
+                            if(index > 2 && !indexSet.contains(index)){
+                                area += areaMap.get(index);
+                                indexSet.add(index);
+                            }   
+                        }
+                    }
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        
+        
+        return maxArea;
+    }
+    
+    public int dfs(int x, int y, int[][] grid, int row, int col, int index){
+        int area = 0;
+        grid[x][y] = index;
+        for(int k=0;k<4;k++){
+            int i = x+dr[k];
+            int j = y+dc[k];
+            if(isValid(i,j,grid,row,col)&& grid[i][j]==1){
+                 area += dfs(i,j,grid,row,col,index);
+            } 
+        }
+        return 1 + area;
+    }
+    
+    public boolean isValid(int x, int y, int[][] grid,int row, int col){
+        if(x < 0 || x >= row || y < 0 || y >= col ){
+            return false;
+        }
+        return true;
+    }
+}
+
+
+
