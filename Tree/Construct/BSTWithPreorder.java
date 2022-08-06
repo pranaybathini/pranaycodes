4e34b96ce46b52ea76fcc72d9ce2314b8c7abb39
Pranay Bathini <pranaygoud36@gmail.com>
2022-08-06 12:16:22 +0530

    Create BSTWithPreorder.java

---
+++ Tree/Construct/BSTWithPreorder.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, Integer.MAX_VALUE, new int[]{0});
+    }
+    
+    public TreeNode build(int[] preorder, int bound, int[] index){
+        if(preorder.length == index[0] || preorder[index[0]]>bound) return null;
+        TreeNode root = new TreeNode(preorder[index[0]++]);
+        root.left = build(preorder,root.val,index);
+        root.right = build(preorder,bound,index);
+        return root;
+    }
+}
