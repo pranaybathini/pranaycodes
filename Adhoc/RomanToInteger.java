c95c3bfd638c7b65aae6d6a4b0dfd111f14ccf92
Pranay Bathini <pranaygoud36@gmail.com>
2022-08-06 12:48:57 +0530

    Create RomanToInteger.java

---
+++ Adhoc/RomanToInteger.java
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/roman-to-integer
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
+
+        int sum = 0;
+        int prev = map.get(s.charAt(0));
+        int next = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            next = map.get(s.charAt(i));
+
+            if (prev < next) {
+                sum -= prev;
+            } else {
+                sum += prev;
+            }
+
+            //udpare prev because it is like sliding window
+            prev = next;
+        }
+
+        sum += prev;//corner case when only one digit, we need to let sum = prev, so we add prev, not next
+        return sum;
+    }
+}
